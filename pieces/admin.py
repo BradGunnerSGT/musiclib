841bd06ab462026cc03fa3821c78d2e9e1a423a9
Mark McCoy <mark@mccoyfam.net>
2013-02-05 17:54:23 -0600

    added publisher, composer, etc

--- pieces/admin.py
+++ pieces/admin.py
@@ -1,8 +1,18 @@
+from pieces.models import Publisher
+from pieces.models import Composer
 from pieces.models import Piece
 from pieces.models import Conductor
 from pieces.models import Performance
 from django.contrib import admin
 
+
+
+class ConductorAdmin(admin.ModelAdmin):
+    # change the way the object is displayed on the admin site listing
+    pass
+
+admin.site.register(Publisher)
+admin.site.register(Composer)
 admin.site.register(Piece)
-admin.site.register(Conductor)
+admin.site.register(Conductor, ConductorAdmin)
 admin.site.register(Performance)
--- pieces/models.py
+++ pieces/models.py
@@ -1,21 +1,58 @@
 from django.db import models
 
 # Create your models here.
+class Composer(models.Model):   
+    first_name = models.CharField(max_length=200)
+    middle_name = models.CharField(max_length=200, null = True, blank = True)
+    last_name = models.CharField(max_length=200)
+    prefix = models.CharField(max_length=20, null = True, blank = True)
+    suffix = models.CharField(max_length=20, null = True, blank = True)
+
+    def name(self):
+        return ("%s %s" % (self.first_name, self.last_name))
+
+    def reversed_name(self, obj):
+        return ("%s %s" % (self.last_name, self.first_name))
+
+    def __unicode__(self):
+        return ("%s %s" % (self.first_name, self.last_name))
+
+class Publisher(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return ("%s" % (title,))
+
+
 class Piece(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    publisher = models.ForeignKey(Publisher)
+    composer = models.ForeignKey(Composer)
+    
 
 class Conductor(models.Model):
     first_name = models.CharField(max_length=200)
-    middle_name = models.CharField(max_length=200, null = True)
+    middle_name = models.CharField(max_length=200, null = True, blank = True)
     last_name = models.CharField(max_length=200)
-    prefix = models.CharField(max_length=20, null = True)
-    suffix = models.CharField(max_length=20, null = True)
+    prefix = models.CharField(max_length=20, null = True, blank = True)
+    suffix = models.CharField(max_length=20, null = True, blank = True)
+
+    def name(self):
+        return ("%s %s" % (self.first_name, self.last_name))
+
+    def reversed_name(self, obj):
+        return ("%s %s" % (self.last_name, self.first_name))
+
+    def __unicode__(self):
+        return ("%s %s" % (self.first_name, self.last_name))
+
+        
 
 class Performance(models.Model):
     piece = models.ForeignKey(Piece)
     conductor = models.ForeignKey(Conductor)
     date = models.DateTimeField('date performed')
+    notes = models.TextField(blank = True, null = True)
 
-    
-    
+
